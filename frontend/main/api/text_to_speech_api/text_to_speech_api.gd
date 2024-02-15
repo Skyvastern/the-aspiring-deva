beec75f3dd5fc9f8610008110ba1205b531e58c4
Arpit Srivastava <skyvastern@gmail.com>
2024-02-15 23:19:48 +0530

    In frontend, use backend's api to carry out text to speech

--- frontend/main/api/text_to_speech_api/text_to_speech_api.gd
+++ frontend/main/api/text_to_speech_api/text_to_speech_api.gd
@@ -2,8 +2,7 @@ extends HTTPRequest
 class_name TextToSpeechAPI
 
 signal processed
-const URL: String = "https://api.openai.com/v1/audio/speech"
-const MP3_PATH: String = "C:\\Users\\Arpit Srivastava\\Desktop\\Audio\\output.mp3"
+const URL: String = "http://127.0.0.1:8000/text-to-speech/"
 const OGG_PATH: String = "C:\\Users\\Arpit Srivastava\\Desktop\\Audio\\output.ogg"
 
 
@@ -12,14 +11,9 @@ func _ready() -> void:
 
 
 func make_request(value: String) -> void:
-	var headers: PackedStringArray = [
-		"Authorization: Bearer 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",
-		"Content-Type: application/json"
-	]
-	
+	var headers: PackedStringArray = ["Content-Type: application/json"]
 	var request_payload: String = JSON.stringify({
-		"model": "tts-1",
-		"input": value,
+		"text": value,
 		"voice": "echo"
 	})
 	
@@ -27,8 +21,7 @@ func make_request(value: String) -> void:
 
 
 func _on_request_completed(_result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
-	Global.save_file_to_disk(body, MP3_PATH)
-	Global.convert_mp3_to_ogg(MP3_PATH, OGG_PATH)
+	Global.save_file_to_disk(body, OGG_PATH)
 	var audio_stream: AudioStreamOggVorbis = AudioStreamOggVorbis.load_from_file(OGG_PATH)
 	
 	processed.emit(audio_stream)
--- frontend/main/singletons/global/global.gd
+++ frontend/main/singletons/global/global.gd
@@ -1,18 +1,6 @@
 extends Node
 
 
-func convert_mp3_to_ogg(mp3_path: String, ogg_path: String) -> void:
-	var output = []
-	var exit_code = OS.execute("ffmpeg", ["-y", "-i", mp3_path, "-c:a", "libvorbis", ogg_path], output)
-	
-	if exit_code == 0:
-		print("Successfully converted mp3 to ogg!")
-	else:
-		print("Conversion failed with exit code: " + str(exit_code))
-		print("Ffmpeg output: " + str(output))
-
-
-
 func convert_wav_to_mp3(wav_path: String, mp3_path: String) -> void:
 	var output = []
 	var exit_code = OS.execute("ffmpeg", ["-y", "-i", wav_path, mp3_path], output)
