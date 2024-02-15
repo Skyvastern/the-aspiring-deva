aadbb266d9ddc9f97ca9cc6410b44a9d19bcdaf7
Arpit Srivastava <skyvastern@gmail.com>
2024-02-15 23:18:30 +0530

    Add text to speech in backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+import ffmpeg
+import os
+import requests
+from fastapi import FastAPI, HTTPException, Response
+from models import TextToSpeechInput
 
 app = FastAPI()
 
@@ -7,6 +11,51 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/text-to-speech/")
+async def text_to_speech(tts_input: TextToSpeechInput):
+    URL: str = "https://api.openai.com/v1/audio/speech"
+
+    headers = {
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json"
+    }
+
+    request_payload = {
+        "model": "tts-1",
+        "input": tts_input.text,
+        "voice": tts_input.voice
+    }
+    
+    try:
+        response = requests.post(URL, json=request_payload, headers=headers)
+
+        if response.status_code == 200:
+            mp3_audio = response.content
+
+            # Save mp3 to a file
+            with open("audio.mp3", "wb") as mp3_file:
+                mp3_file.write(mp3_audio)
+            
+            # Convert mp3 to ogg
+            ffmpeg.input("audio.mp3").output("audio.ogg", acodec="libvorbis").run()
+
+            # Read ogg file and return it in response
+            with open("audio.ogg", "rb") as ogg_file:
+                ogg_audio = ogg_file.read()
+            
+            # Clean the audio files
+            os.remove("audio.mp3")
+            os.remove("audio.ogg")
+
+            return Response(content=ogg_audio, media_type="audio/ogg")
+        
+        else:
+            raise HTTPException(status_code=400, detail="Error from TTS API")
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
---
+++ backend/models.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+class TextToSpeechInput(BaseModel):
+    text: str
+    voice: str
