6ca139c7ebc477e9b39c960685767401fd1ea9b1
Arpit Srivastava <skyvastern@gmail.com>
2024-02-17 20:55:35 +0530

    Use text generation in npc_speak scene

--- frontend/main/npc/npc_speak/npc_speak.gd
+++ frontend/main/npc/npc_speak/npc_speak.gd
@@ -7,19 +7,24 @@ class_name NPC_Speak
 
 @export_group("References")
 @export_file("*.tscn") var audio_record_scene_path: String
-@export var text_to_speech_api: TextToSpeechAPI
 @export var audio_player: AudioStreamPlayer
+@export var textgen_api: TextgenAPI
+@export var text_to_speech_api: TextToSpeechAPI
+
+var reply: String = ""
 
 
 func _ready() -> void:
 	text_to_speech_api.processed.connect(_on_text_to_speech_api_processed)
+	textgen_api.processed.connect(_on_textgen_api_processed)
 	continue_btn.pressed.connect(_on_continue_btn_pressed)
 	
 	npc_response.text = ""
 
 
 func interpret(question: String) -> void:
-	print(question)
+	npc_response.text = "Interpreting..."
+	textgen_api.make_request(question)
 
 
 func speak(value: String) -> void:
@@ -29,6 +34,13 @@ func speak(value: String) -> void:
 func _on_text_to_speech_api_processed(audio_stream: AudioStreamOggVorbis) -> void:
 	audio_player.stream = audio_stream
 	audio_player.play()
+	
+	npc_response.text = reply
+
+
+func _on_textgen_api_processed(json: Dictionary) -> void:
+	reply = json["message"]["content"]
+	speak(reply)
 
 
 func _on_continue_btn_pressed() -> void:
