ae8bdfa0e9c0e671ee503ff1abc4614ef018494e
Arpit Srivastava <skyvastern@gmail.com>
2024-02-27 23:57:29 +0530

    Hide crosshair when there's an active npc interaction going on

--- frontend/main/player/hud/hud.gd
+++ frontend/main/player/hud/hud.gd
@@ -6,7 +6,17 @@ class_name HUD
 
 func _ready() -> void:
 	Global.main.pause_menu.game_paused.connect(_on_game_paused)
+	Global.active_npc_updated.connect(_on_active_npc_updated)
 
 
 func _on_game_paused(paused: bool) -> void:
-	visible = !paused
+	if paused:
+		visible = false
+	elif paused == false and Global.active_npc != null:
+		visible = false
+	else:
+		visible = true
+
+
+func _on_active_npc_updated(npc: NPC) -> void:
+	visible = npc == null
