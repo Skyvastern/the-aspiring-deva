3e5b71d22c8707b493e0866b59f38ceaaf79137c
Arpit Srivastava <skyvastern@gmail.com>
2024-03-08 20:12:09 +0530

    Update npc's rotation in npc_movement

--- frontend/main/npc/components/npc_movement/npc_movement.gd
+++ frontend/main/npc/components/npc_movement/npc_movement.gd
@@ -18,10 +18,15 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	# Movement
 	var target_pos: Vector3 = current_waypoint.global_position
 	var dir: Vector3 = npc.global_position.direction_to(target_pos)
 	npc.global_position += dir * speed * delta
 	
+	# Rotation
+	npc.target = current_waypoint
+	
+	# Current Waypoint Reached
 	if npc.global_position.distance_to(target_pos) < 1:
 		wait_timer.start()
 		set_physics_process(false)
