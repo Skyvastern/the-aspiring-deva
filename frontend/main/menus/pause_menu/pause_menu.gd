3e4420ac006e714b998d2f84ffefe56a1ae04943
Arpit Srivastava <skyvastern@gmail.com>
2024-03-13 16:26:45 +0530

    Rename exit btn to main menu btn in pause menu

--- frontend/main/menus/pause_menu/pause_menu.gd
+++ frontend/main/menus/pause_menu/pause_menu.gd
@@ -5,7 +5,7 @@ signal game_paused
 
 @export_group("UI")
 @export var resume_btn: Button
-@export var exit_btn: Button
+@export var main_menu_btn: Button
 
 @export_group("References")
 @export_file("*.tscn") var main_menu_scene_path: String
@@ -14,7 +14,7 @@ signal game_paused
 func _ready() -> void:
 	visibility_changed.connect(_on_visibility_changed)
 	resume_btn.pressed.connect(_on_resume_btn_pressed)
-	exit_btn.pressed.connect(_on_exit_btn_pressed)
+	main_menu_btn.pressed.connect(_on_main_menu_btn_pressed)
 	
 	visible = false
 
@@ -33,7 +33,7 @@ func _on_resume_btn_pressed() -> void:
 	visible = false
 
 
-func _on_exit_btn_pressed() -> void:
+func _on_main_menu_btn_pressed() -> void:
 	get_tree().paused = false
 	Global.load_menu(
 		Global.level,
